2ae8bf442a65cbc076e7b3e4de2c1c9a46368852
Cory Rucker <corymrucker@gmail.com>
2019-07-23 01:53:18 +0000

    Styling the Top-Nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,9 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 //= require turbolinks
+//= require popper
+//= require tether
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
