e5ede950974bc44a856e16dd91c40c9b40f40dc9
Cory Rucker <corymrucker@gmail.com>
2019-08-04 23:07:51 +0000

    Next lesson button

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,7 +1,16 @@
 class Section < ApplicationRecord
-	belongs_to :course
-	has_many :lessons
+  belongs_to :course
+  has_many :lessons
 
-    include RankedModel
-    ranks :row_order, with_same: :course_id
+  include RankedModel
+  ranks :row_order, with_same: :course_id
+
+  def next_lesson
+    lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+    if lesson.blank? && section.next_section
+      return section.next_section.lessons.rank(:row_order).first
+    end
+    return lesson
+  end
+
 end
